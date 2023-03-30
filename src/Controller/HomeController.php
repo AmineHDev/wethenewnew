04e7e80ec77ec644837fa0b050a3bd13e98e710e
Amine Hammouti <aminehammouti@MacBook-Pro-de-Amine.local>
2023-03-30 17:30:23 +0200

    heroku

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -15,6 +15,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
